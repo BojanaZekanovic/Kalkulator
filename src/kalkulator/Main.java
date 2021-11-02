84d1216240ae0f36f23ddb6ef4f231e78fa6746d
Bojana Zekanović <zekanovicbojana4@gmail.com>
2021-11-02 23:49:37 +0100

    Initial read line

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,9 +1,16 @@
 package kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner src = new Scanner(System.in);
+		String komanda=" ";
+		do {
+		 komanda=src.nextLine();
+			}while(!komanda.equals("End"));
 
 	}
 
