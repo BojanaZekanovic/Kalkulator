b4e45b4a4651093c78d41a990c652b556ad5900d
Bojana Zekanović <zekanovicbojana4@gmail.com>
2021-11-04 00:07:39 +0100

    Implement addition

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -17,6 +17,22 @@ public class Main {
 		 		if(kom.length!=3) {
 		 			continue;
 		 		}
+		 		int x= Integer.parseInt(kom[0]);
+		 		int y= Integer.parseInt(kom[2]);
+		 		int z=0;
+		 		switch (kom[1]) {
+				case "+": 
+					
+					z=x+y;
+					break;
+				
+				default:
+					System.out.println("Operand nije podrzan");
+					continue;
+					
+				} 
+		 		
+		 		System.out.println("rezultat" + z);
 		 		
 			}while(!komanda.equals("End"));
 
